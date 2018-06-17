7d3872fbb2ba6f18e65f02cc6577acabba0bc775
Rupesh Kumar Saw <38566315+sawrupesh04@users.noreply.github.com>
2018-06-17 21:04:57 +0530

    Add files via upload

---
+++ turtle/t.py
@@ -0,0 +1,8 @@
+from turtle import *
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(59)
+    speed(1000)
